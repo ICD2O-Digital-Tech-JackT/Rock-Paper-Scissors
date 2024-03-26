a9f7f1dfe25251efb672461efbb68689be3cbea0
Jack Turcotte <159164296+JackTurcs@users.noreply.github.com>
2024-03-26 15:04:34 +0000

    First Part Made

--- js/script.js
+++ js/script.js
@@ -1,5 +1,92 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+let GameHasStarted = false;
+let UserResponseInput;
+let ConfimationButton;
+let PlayerChoiceNumber;
+let Warning;
+let Tie = false;
+
+function PlayerChosenOption(){
+  let ValidRe = false
+  if (UserResponseInput.value=="Scissors"||UserResponseInput.value=="Rock"||UserResponseInput.value=="Paper"){
+    let Message = document.createElement("p");
+    Message.innerHTML="You chose "+UserResponseInput.value;
+    document.body.appendChild(Message);
+    ValidRe=true
+
+    if (UserResponseInput.value=="Rock"){
+      PlayerChoiceNumber=1
+    }
+    if (UserResponseInput.value=="Paper"){
+      PlayerChoiceNumber=2
+    }
+    if (UserResponseInput.value=="Scissors"){
+      PlayerChoiceNumber=3
+    }
+
+  } else{
+    let Message = document.createElement("p");
+    Message.style.color="Red"
+    Message.innerHTML=UserResponseInput.value+" is not a valid option, please check your spelling";
+    document.body.appendChild(Message);
+  }
+
+  if (ValidRe==true){
+    let Win = false
+    let OpponentChoice = Math.random(1,3);
+    if (OpponentChoice==1){
+      let OpponentChoiceMsg = document.createElement("p");
+      OpponentChoice.innerHTML="Your opponent chose Rock";
+      if (OpponentChoice==3){
+        Win=true
+      }
+    }
+    if (OpponentChoice==2){
+      let OpponentChoiceMsg = document.createElement("p");
+      OpponentChoice.innerHTML="Your opponent chose Paper";
+      if (OpponentChoice==1){
+        Win=true
+      }
+    }
+    if (OpponentChoice==3){
+      let OpponentChoiceMsg = document.createElement("p");
+      OpponentChoice.innerHTML="Your opponent chose Scissors";
+      if (OpponentChoice==2){
+        Win=true
+      }
+    }
+    if (OpponentChoice==PlayerChoiceNumber){
+      Tie=true
+      Win=true
+    }
+
+    if (Win==true){
+      if (Tie==false){
+        let OpponentChoiceMsg = document.createElement("p");
+        OpponentChoice.innerHTML="You win!"+UserResponseInput.value;
+      } else{
+        let OpponentChoiceMsg = document.createElement("p");
+        OpponentChoice.innerHTML="You tied /:"+UserResponseInput.value;
+      }
+    } else{
+      let OpponentChoiceMsg = document.createElement("p");
+      OpponentChoice.innerHTML="You did not win, Try again"+UserResponseInput.value;
+    }
+  }
+}
+
+
+function InitiateGame(){
+  if (GameHasStarted==false){
+    GameHasStarted=true;
+    document.getElementById("Text").innerHTML+="This is rock paper scissors, input your choice below: <br><br>";
+    let PlayerChoice = document.createElement("input",type="text",id="PlayerChoice");
+    UserResponseInput=PlayerChoice;
+    document.body.appendChild(PlayerChoice); 
+    let ButtonConfirm = document.createElement("button");
+    ConfimationButton=ButtonConfirm;
+    ButtonConfirm.innerHTML="Confirm";
+    ButtonConfirm.addEventListener('click', PlayerChosenOption);
+    document.body.appendChild(ButtonConfirm);
+  }
+}
+
