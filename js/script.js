2dc71486c1834e0011c5506823d491b96fcdd6fa
Jack Turcotte <159164296+JackTurcs@users.noreply.github.com>
2024-04-03 14:22:23 +0000

    gfdseghgvfdsew

--- js/script.js
+++ js/script.js
@@ -5,86 +5,97 @@ let PlayerChoiceNumber;
 let Warning;
 let Tie = false;
 
-function PlayerChosenOption(){
-  let ValidRe = false
-  if (UserResponseInput.value=="Scissors"||UserResponseInput.value=="Rock"||UserResponseInput.value=="Paper"){
-    let Message = document.createElement("p");
-    Message.innerHTML="You chose "+UserResponseInput.value;
-    document.body.appendChild(Message);
-    ValidRe=true
+function ValidResponse(ValidRe) {
+  let Win = false
+  let OpponentChoice = Math.floor(Math.random() * 1) + 1;
+  document.getElementById("MSG").innerHTML = OpponentChoice
+  if (OpponentChoice == 1) {
+    let OpponentChoiceMsg = document.createElement("p");
+    document.body.appendChild(OpponentChoiceMsg)
+    OpponentChoice.innerHTML = "Your opponent chose Rock";
+    if (OpponentChoice == 3) {
+      Win = true
+    }
+  }
+  if (OpponentChoice == 2) {
+    let OpponentChoiceMsg = document.createElement("p");
+    document.body.appendChild(OpponentChoiceMsg)
+    OpponentChoiceMsg.innerHTML = "Your opponent chose Paper";
+    if (OpponentChoice == 1) {
+      Win = true
+    }
+  }
+  if (OpponentChoice == 3) {
+    let OpponentChoiceMsg = document.createElement("p");
+    document.body.appendChild(OpponentChoiceMsg)
+    OpponentChoice.innerHTML = "Your opponent chose Scissors";
+    if (OpponentChoice == 2) {
+      Win = true
+    }
+  }
+  if (OpponentChoice == PlayerChoiceNumber) {
 
-    if (UserResponseInput.value=="Rock"){
-      PlayerChoiceNumber=1
-    }
-    if (UserResponseInput.value=="Paper"){
-      PlayerChoiceNumber=2
-    }
-    if (UserResponseInput.value=="Scissors"){
-      PlayerChoiceNumber=3
-    }
-
-  } else{
-    let Message = document.createElement("p");
-    Message.style.color="Red"
-    Message.innerHTML=UserResponseInput.value+" is not a valid option, please check your spelling";
-    document.body.appendChild(Message);
+    Tie = true
+    Win = true
   }
 
-  if (ValidRe==true){
-    let Win = false
-    let OpponentChoice = Math.random(1,3);
-    if (OpponentChoice==1){
+  if (Win == true) {
+    if (Tie == false) {
       let OpponentChoiceMsg = document.createElement("p");
-      OpponentChoice.innerHTML="Your opponent chose Rock";
-      if (OpponentChoice==3){
-        Win=true
-      }
-    }
-    if (OpponentChoice==2){
+      document.body.appendChild(OpponentChoiceMsg)
+      OpponentChoice.innerHTML = "You win!" + UserResponseInput.value;
+    } else {
       let OpponentChoiceMsg = document.createElement("p");
-      OpponentChoice.innerHTML="Your opponent chose Paper";
-      if (OpponentChoice==1){
-        Win=true
-      }
+      document.body.appendChild(OpponentChoiceMsg)
+      OpponentChoice.innerHTML = "You tied /:" + UserResponseInput.value;
     }
-    if (OpponentChoice==3){
-      let OpponentChoiceMsg = document.createElement("p");
-      OpponentChoice.innerHTML="Your opponent chose Scissors";
-      if (OpponentChoice==2){
-        Win=true
-      }
-    }
-    if (OpponentChoice==PlayerChoiceNumber){
-      Tie=true
-      Win=true
-    }
-
-    if (Win==true){
-      if (Tie==false){
-        let OpponentChoiceMsg = document.createElement("p");
-        OpponentChoice.innerHTML="You win!"+UserResponseInput.value;
-      } else{
-        let OpponentChoiceMsg = document.createElement("p");
-        OpponentChoice.innerHTML="You tied /:"+UserResponseInput.value;
-      }
-    } else{
-      let OpponentChoiceMsg = document.createElement("p");
-      OpponentChoice.innerHTML="You did not win, Try again"+UserResponseInput.value;
-    }
+  } else {
+    let OpponentChoiceMsg = document.createElement("p");
+    document.body.appendChild(OpponentChoiceMsg)
+    OpponentChoice.innerHTML = "You did not win, Try again" + UserResponseInput.value;
   }
 }
 
 
-function InitiateGame(){
-  if (GameHasStarted==false){
-    GameHasStarted=true;
-    document.getElementById("Text").innerHTML+="This is rock paper scissors, input your choice below: <br><br>";
-    let PlayerChoice = document.createElement("input",type="text",id="PlayerChoice");
-    UserResponseInput=PlayerChoice;
-    document.body.appendChild(PlayerChoice); 
+function PlayerChosenOption() {
+  let ValidRe = false
+  if (UserResponseInput.value == "Scissors" || UserResponseInput.value == "Rock" || UserResponseInput.value == "Paper") {
+    let Message = document.createElement("p");
+    Message.id = "MSG"
+    //Message.innerHTML="You chose "+UserResponseInput.value;
+    document.body.appendChild(Message);
+    ValidRe = true
+
+    if (UserResponseInput.value == "Rock") {
+      PlayerChoiceNumber = 1
+    }
+    if (UserResponseInput.value == "Paper") {
+      PlayerChoiceNumber = 2
+    }
+    if (UserResponseInput.value == "Scissors") {
+      PlayerChoiceNumber = 3
+    }
+    ValidResponse(ValidRe)
+  } else {
+    let Message = document.createElement("p");
+    Message.style.color = "Red"
+    Message.innerHTML = UserResponseInput.value + " is not a valid option, please check your spelling";
+    document.body.appendChild(Message);
+  }
+}
+
+
+function InitiateGame() {
+  if (GameHasStarted == false) {
+    GameHasStarted = true;
+    document.getElementById("Text").innerHTML += "This is rock paper scissors, input your choice below: <br><br>";
+    let PlayerChoice = document.createElement("input", type = "text", id = "PlayerChoice");
+    UserResponseInput = PlayerChoice;
+    document.body.appendChild(PlayerChoice);
     let ButtonConfirm = document.createElement("button");
-    ConfimationButton=ButtonConfirm;
-    ButtonConfirm.innerHTML="Confirm";
+    ConfimationButton = ButtonConfirm;
+    ButtonConfirm.innerHTML = "Confirm";
+    document.getElementById("Text").innerHTML = Math.random(1,100)
     ButtonConfirm.addEventListener('click', PlayerChosenOption);
     document.body.appendChild(ButtonConfirm);
   }
